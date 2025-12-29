2339b0faf4535106539fcf36e709fe7acb5b3d11
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2025-12-29 08:15:45 -0500

    starter scss added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import './App.css'
+import "./App.css";
 
 export default function App() {
   return (
     <div>
-      Features
+      <div data-text-preset="heading">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </div>
+      <div data-text-preset="body">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </div>
+      <div data-text-preset="button">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </div>
+      <div data-text-preset="nav">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </div>
+
+      {/* Features
       Company
       Careers
       About
@@ -16,7 +29,7 @@ export default function App() {
       Get your team in sync, no matter your location. Streamline processes, 
       create team rituals, and watch productivity soar.
 
-      Learn more
+      Learn more */}
     </div>
-  )
+  );
 }
